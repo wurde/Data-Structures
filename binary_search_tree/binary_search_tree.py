bf96e8e5804dcb3e921bf82528b4a945ab634e48
Andy Bettisworth <andy@accreu.com>
2019-09-18 16:24:56 -0500

    Set TODO for binary search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -2,8 +2,9 @@
 # Dependencies
 #
 
+import os
 import sys
-sys.path.append('../queue_and_stack')
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../queue_and_stack'))
 from dll_queue import Queue
 from dll_stack import Stack
 
@@ -12,19 +13,23 @@ from dll_stack import Stack
 #
 
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        # TODO add value.
+        pass
 
-  def contains(self, target):
-    pass
+    def contains(self, target):
+        # TODO check if target exists.
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        # TODO return the max value.
+        pass
 
-  def for_each(self, cb):
-    pass
+    def for_each(self, cb):
+        # TODO run callback on all nodes.
+        pass
